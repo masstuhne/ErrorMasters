5bb08db35d073590e072b3150fa7f0215336ea6a
Dario <darioohuic@gmail.com>
2023-10-24 18:39:15 +0200

    Added initial dev versions of backend and frontend

---
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/CookbookedApplication.java
@@ -0,0 +1,12 @@
+package com.fer.progi.errormasters.Cookbooked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookbookedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookbookedApplication.class, args);
+	}
+}
---
+++ IzvorniKod/backend/Cookbooked/src/test/java/com/fer/progi/errormasters/Cookbooked/CookbookedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer.progi.errormasters.Cookbooked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbookedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
