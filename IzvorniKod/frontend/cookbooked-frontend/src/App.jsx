e30c47bbcfb23227f046da07355586dbabf08f72
Dario <darioohuic@gmail.com>
2023-10-26 16:36:28 +0200

    Changed frontend to Vite

---
+++ IzvorniKod/frontend/cookbooked-frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import './index.css'
+
+function App() {
+
+    return (
+
+        <div className="App">
+
+            <h1>React App</h1>
+
+        </div>
+
+    );
+
+}
+
+
+
+
+export default App;
