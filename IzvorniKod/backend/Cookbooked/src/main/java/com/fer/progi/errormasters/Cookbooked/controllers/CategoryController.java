e0318d3bfd8ed25eec7c86d86067ad5b761f0609
lukaivekovic <luka.ivekovic22@gmail.com>
2023-11-01 17:41:58 +0100

    Implementing CategoryService and CategoryController

--- IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/controllers/CategoryController.java
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/controllers/CategoryController.java
@@ -1,23 +1,29 @@
 package com.fer.progi.errormasters.Cookbooked.controllers;
 
+import com.fer.progi.errormasters.Cookbooked.entities.Category;
+import com.fer.progi.errormasters.Cookbooked.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping
-    public String GetCategories(){
-        return "Returning all categories";
+    public List<Category> GetCategories(){
+        return categoryService.getAllCategories();
     }
 
     @GetMapping("{id}")
-    public String GetCategory(@PathVariable int id){
-        return "Returning the category with id = " + id;
+    public Optional<Category> GetCategory(@PathVariable int id){
+        return categoryService.getCategory(id);
     }
 }
--- IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/services/impl/CategoryServiceImpl.java
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/services/impl/CategoryServiceImpl.java
@@ -1,4 +1,30 @@
 package com.fer.progi.errormasters.Cookbooked.services.impl;
 
-public class CategoryServiceImpl {
+import com.fer.progi.errormasters.Cookbooked.entities.Category;
+import com.fer.progi.errormasters.Cookbooked.repositories.CategoryRepostiory;
+import com.fer.progi.errormasters.Cookbooked.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepostiory categoryRepostiory;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepostiory categoryRepostiory) {
+        this.categoryRepostiory = categoryRepostiory;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepostiory.findAll();
+    }
+
+    @Override
+    public Optional<Category> getCategory(int id) {
+        return categoryRepostiory.findById(id);
+    }
 }
