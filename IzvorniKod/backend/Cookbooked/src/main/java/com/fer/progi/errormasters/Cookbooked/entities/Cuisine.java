05d56629031d16f12f0c3bdc478b3c407ca7c221
Dario <darioohuic@gmail.com>
2023-11-01 17:09:26 +0100

    Added entities

---
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/entities/Cuisine.java
@@ -0,0 +1,22 @@
+package com.fer.progi.errormasters.Cookbooked.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Cuisine")
+@Getter
+@Setter
+
+public class Cuisine {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+
+}
---
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/entities/Ingredient.java
@@ -0,0 +1,19 @@
+package com.fer.progi.errormasters.Cookbooked.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Ingredient")
+@Getter
+@Setter
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
