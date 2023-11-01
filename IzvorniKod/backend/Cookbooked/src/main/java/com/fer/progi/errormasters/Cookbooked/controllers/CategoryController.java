1c74e734dafd058a205083e6eaf38993dea03aa1
lukaivekovic <luka.ivekovic22@gmail.com>
2023-11-01 17:55:19 +0100

    Implementing CategoryController and IngredientController

--- IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/controllers/CategoryController.java
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/controllers/CategoryController.java
@@ -1,8 +1,10 @@
 package com.fer.progi.errormasters.Cookbooked.controllers;
 
 import com.fer.progi.errormasters.Cookbooked.entities.Category;
+import com.fer.progi.errormasters.Cookbooked.entities.Ingredient;
 import com.fer.progi.errormasters.Cookbooked.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +20,24 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public List<Category> GetCategories(){
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<Category>> GetCategories(){
+        List<Category> categories = categoryService.getAllCategories();
+
+        if (categories.isEmpty()){
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(categories);
+        }
     }
 
     @GetMapping("{id}")
-    public Optional<Category> GetCategory(@PathVariable int id){
-        return categoryService.getCategory(id);
+    public ResponseEntity<Category> GetCategory(@PathVariable int id){
+        Optional<Category> category = categoryService.getCategory(id);
+
+        if (category.isPresent()){
+            return ResponseEntity.ok(category.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
