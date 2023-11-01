e372c7a88343ee65b398b96da43114b2854555da
Dario <darioohuic@gmail.com>
2023-11-01 17:41:58 +0100

    Added service implementation

--- IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/services/IngredientService.java
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/services/IngredientService.java
@@ -1,4 +1,17 @@
 package com.fer.progi.errormasters.Cookbooked.services;
 
+import com.fer.progi.errormasters.Cookbooked.entities.Category;
+import com.fer.progi.errormasters.Cookbooked.entities.Ingredient;
+import com.fer.progi.errormasters.Cookbooked.repositories.IngredientRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IngredientService {
+
+    public List<Ingredient> getAllIngredients();
+
+    public Optional<Ingredient> getIngredientById(Integer id);
+
+
 }
--- IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/services/impl/CuisineServiceImpl.java
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/services/impl/CuisineServiceImpl.java
@@ -1,4 +1,29 @@
 package com.fer.progi.errormasters.Cookbooked.services.impl;
 
-public class CuisineServiceImpl {
+import com.fer.progi.errormasters.Cookbooked.entities.Cuisine;
+import com.fer.progi.errormasters.Cookbooked.repositories.CuisineRepository;
+import com.fer.progi.errormasters.Cookbooked.services.CuisineService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CuisineServiceImpl implements CuisineService {
+
+
+    private final CuisineRepository cuisineRepository;
+
+
+    @Override
+    public List<Cuisine> getAllCuisines() {
+        return cuisineRepository.findAll();
+    }
+
+    @Override
+    public Optional<Cuisine> getCuisineById(Integer id) {
+        return cuisineRepository.findById(id);
+    }
 }
