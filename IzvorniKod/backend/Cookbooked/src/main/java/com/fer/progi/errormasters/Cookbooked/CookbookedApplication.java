f24f97cd0950f72d11a6fb200f88ce9a1880ecc0
Dario <darioohuic@gmail.com>
2023-11-01 18:31:26 +0100

    Added temporary exclude spring security

--- IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/CookbookedApplication.java
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/CookbookedApplication.java
@@ -3,7 +3,9 @@ package com.fer.progi.errormasters.Cookbooked;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+})
 public class CookbookedApplication {
 
 
