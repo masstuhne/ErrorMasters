27da7fb80f2694152e24bad6cfebded1d561872c
Dario <darioohuic@gmail.com>
2023-11-12 17:13:10 +0100

    Added security config

--- IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/config/SecurityConfig.java
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.fer.progi.errormasters.Cookbooked.config;
 
 import com.fer.progi.errormasters.Cookbooked.services.security.JpaUserDetailsService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,14 +16,17 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableMethodSecurity()
+@EnableWebSecurity
 public class SecurityConfig {
 
-
-
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-
-
+        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                .requestMatchers("/ingredients").permitAll()
+                .requestMatchers("/categories").permitAll()
+                .requestMatchers("/cuisines").permitAll()
+                .anyRequest().authenticated()
+        );
 
         http.csrf(AbstractHttpConfigurer::disable);
         http.addFilterBefore(null, UsernamePasswordAuthenticationFilter.class);
