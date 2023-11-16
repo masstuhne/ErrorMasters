0c9e3402eae53571c3ba04f59376197115e1e317
ValentinaIvanic <ivanic.valentina2@gmail.com>
2023-11-16 17:58:01 +0100

    1 bug i NavBar nakon prijave/odjave

--- IzvorniKod/frontend/cookbooked-frontend/src/components/UserLog.jsx
+++ IzvorniKod/frontend/cookbooked-frontend/src/components/UserLog.jsx
@@ -30,9 +30,9 @@ function UserLog({children}) {
           <li>
             <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Spremljeni Recepti</a>
           </li>
-          <Link to="/" onClick={LogOut}>
+          <Link to="/" onClick={LogOut} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
           <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Odjava</a>
+            Odjava
           </li>
           </Link>
         </ul>
