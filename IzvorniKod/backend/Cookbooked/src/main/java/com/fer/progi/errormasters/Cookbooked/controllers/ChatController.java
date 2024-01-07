d292281a4b116b6fcc03dca209364fdd7ed86430
lukaivekovic <luka.ivekovic22@gmail.com>
2024-01-07 17:03:37 +0100

    Adding websocket implementation for chat funcionlity

---
+++ IzvorniKod/backend/Cookbooked/src/main/java/com/fer/progi/errormasters/Cookbooked/controllers/ChatController.java
@@ -0,0 +1,31 @@
+package com.fer.progi.errormasters.Cookbooked.controllers;
+
+import com.fer.progi.errormasters.Cookbooked.models.payloads.ChatMessageModel;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/cookbooked")
+    @PreAuthorize("isAuthenticated()")
+    @SecurityRequirement(name = "jwt")
+    public ChatMessageModel register(@Payload ChatMessageModel chatMessageModel, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessageModel.getSender());
+        return chatMessageModel;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/cookbooked")
+    @PreAuthorize("isAuthenticated()")
+    @SecurityRequirement(name = "jwt")
+    public ChatMessageModel sendMessage(@Payload ChatMessageModel chatMessageModel) {
+        return chatMessageModel;
+    }
+}
