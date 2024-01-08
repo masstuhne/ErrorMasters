cf071cdecedaae4e0ee25eb450f5b6b638a42b50
ValentinaIvanic <ivanic.valentina2@gmail.com>
2024-01-08 17:54:56 +0100

    Fixed css for MyRecipesList

--- IzvorniKod/frontend/cookbooked-frontend/src/components/MyRecipesList.jsx
+++ IzvorniKod/frontend/cookbooked-frontend/src/components/MyRecipesList.jsx
@@ -36,38 +36,39 @@ function MyRecipesList() {
       ];
 
     return (
-            <div className="flex items-center justify-center flex-col min-h-screen gap-10">
-                <div className="flex items-center justify-center flex-row gap-10 min-w-[100rem]">
-                    <div className="flex items-center justify-content: flex-end flex-col gap-10 min-w-[50rem]">
+            <div className="flex items-center justify-center flex-col mt-[10rem] w-screen gap-10">
+                <div className="flex items-center justify-center flex-row min-w-[100rem]">
+                    <div className="flex items-center justify-content: flex-end  gap-10 ">
                         <h2 className="mb-2 text-4xl font-semibold text-gray-900 dark:text-white">Moji recepti:</h2> 
-                    </div>
-                    <div className="flex items-center justify-end flex-col gap-10 min-w-[50rem]">
-                        <Link to="/novi_recept">
-                            <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Izradi novi recept</button>
-                        </Link>
                     </div>
                 </div>
                 
-                <div className="flex min-w-[90rem] min-h-[35rem] flex-col gap-4">
+                <div className="flex w-screen min-h-[35rem] flex-col items-center justify-center gap-4">
 
-                    <ul className="max-w-md space-y-1 text-2xl text-gray-500 list-disc  dark:text-gray-400">
+                    <ul className="w-2/5 space-y-1 text-2xl text-gray-500 list-disc  dark:text-gray-400">
                         {recepti.map(el => (
-                            <li key={el.id} className="py-3 sm:py-4">
+                            <li key={el.id} className="py-3 sm:py-4 border-b border-gray-300">
                                 <div className="flex items-center space-x-4 rtl:space-x-reverse">
-                                    <div className="flex-1 min-w-0">
-                                        <Link to={`/recept/${el.id}`} className="text-2xl font-medium text-gray-900 truncate dark:text-white">
+                                    <div className="w-3/6">
+                                        <h1 className="text-2xl font-medium text-gray-900 truncate dark:text-white">
                                         {el.ime}
-                                        </Link>
+                                        </h1>
                                         <p className="text-lg text-gray-500 truncate dark:text-gray-400">
                                         - {el.kategorija}
                                         </p>
                                     </div>
-                                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                    <div className="inline-flex w-1/6 items-center text-base font-semibold text-gray-900 dark:text-white">
                                         kuhanje: {el.vrijeme} min
+                                    </div>
+                                    <div className="flex justify-center w-2/6">
+                                        <Link to={`/recept/${el.id}`}>
+                                          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pročitaj više</button>
+                                        </Link>
                                     </div>
                                 </div>
                             </li>
                         ))}
+                        
                     </ul>
                 </div>
             </div>
