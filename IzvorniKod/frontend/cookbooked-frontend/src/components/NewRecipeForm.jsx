9de838ae48e1ef88684ba9255cf674f2df476144
DominikOzvald <147696759+DominikOzvald@users.noreply.github.com>
2024-01-10 12:12:54 +0100

    Prikaz mojih recpata, prikaz pojedinog recepta, post dodoja recep, prikaz spremljenih recepata

--- IzvorniKod/frontend/cookbooked-frontend/src/components/NewRecipeForm.jsx
+++ IzvorniKod/frontend/cookbooked-frontend/src/components/NewRecipeForm.jsx
@@ -7,6 +7,39 @@ function NewRecipeForm() {
     const [cuisines, setCuisines] = useState([]);
     const [categories, setCategories] = useState([]);
     const [ingredients, setIngredients] = useState([]);
+    const RCP_URL="http://localhost:8080/api/v1/recipes/add"
+
+    const handleSubmit= async (e)=>{
+        e.preventDefault() 
+        try{
+            let token=localStorage.getItem('user_ret')
+            let time=parseInt(cookingTime)
+            let ingredientIds=userChoice.map(ingr => ingr.value)
+            const formData= new FormData()
+            
+            if(images!='') {formData.append('imageFiles',images)}
+            if(video!='') formData.append('videoFile',video)
+            formData.append('title',titile)
+            formData.append('description',descripton)
+            formData.append('cookingTime',time)
+            formData.append('categoryId',selectedCategory)
+            formData.append('cuisineId',selectedCuisine)
+            formData.append('ingredients',ingredientIds)
+            formData.append('tags',[])
+
+            const response= await axios.post(RCP_URL,formData,{headers :{"Content-Type":"multipart/form-data",
+                        Authorization: `Bearer ${token}`,
+            }})
+
+            console.log(response.status)
+            console.log(response.data)
+          
+        }
+        catch(err){
+          console.log(err);
+        }
+      }
+    
 
     const url1 = 'http://localhost:8080/api/v1/cuisines';
     const url2 = 'http://localhost:8080/api/v1/categories';
@@ -52,14 +85,23 @@ function NewRecipeForm() {
       }));
 
     const [userChoice, setUserChoice] = useState([]);
+    const [selectedCategory,setSelectedCategory]= useState([])
+    const [selectedCuisine,setSelectedCousine]= useState([])
+    const [titile,setTitle]=useState("")
+    const [descripton,setDescriptiom]= useState("")
+    const [cookingTime,setCookinTime]= useState('0')
+    const [images,setImages]=useState('')
+    const [video,setVideo]=useState('')
+
+      
 
     return (
         <div className="flex items-center justify-center mt-[10rem] w-screen">
-            <form className="flex items-center justify-center max-w-100vh min-h-[40rem] w-5/6 flex-col gap-4 ">
+            <form onSubmit={handleSubmit} className="flex items-center justify-center max-w-100vh min-h-[40rem] w-5/6 flex-col gap-4 ">
                 <div className="flex w-full min-h-[40rem] flex-row gap-4">
                     <div className='w-1/2 flex flex-col gap-4'>
                         <div className="relative max-w-[40rem]">
-                            <input type="text" id="naslov_recepta" aria-describedby="floating_helper_text" className="relative max-w-[40rem] w-full block rounded-t-lg px-10.5 pb-2.5 pt-5 text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                            <input onChange={e=> setTitle(e.target.value)} type="text" id="naslov_recepta" aria-describedby="floating_helper_text" className="relative max-w-[40rem] w-full block rounded-t-lg px-10.5 pb-2.5 pt-5 text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                             <label htmlFor="naslov_recepta" className="absolute text-x1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto" required>Naslov recepta</label>
                         </div>
                         <div className="relative max-w-[40rem]">
@@ -70,6 +112,8 @@ function NewRecipeForm() {
                                 className="basic-single"
                                 classNamePrefix="select"
                                 placeholder="Odaberi kuhinju"
+                                onChange={(choice) => {setSelectedCousine(choice);
+                                    console.log(choice)}}
                             />
                         </div>
                         <div className="relative max-w-[40rem]">
@@ -80,6 +124,8 @@ function NewRecipeForm() {
                                 className="basic-single"
                                 classNamePrefix="select"
                                 placeholder="Odaberi kategoriju"
+                                onChange={(choice) => {setSelectedCategory(choice);
+                                    console.log(choice)}}
                             />
                         </div>
                         <div className="relative max-w-[40rem]">
@@ -90,7 +136,8 @@ function NewRecipeForm() {
                                 className="basic-multi-select"
                                 classNamePrefix="select"
                                 placeholder="Odaberi sastojke"
-                                onChange={(choice) => setUserChoice(choice)}
+                                onChange={(choice) => {setUserChoice(choice);
+                                    console.log(choice)}}
                             />
                         </div>
                         <div className='flex flex-col gap-4 max-w-[40rem]'>
@@ -113,11 +160,11 @@ function NewRecipeForm() {
                     <div className='w-1/2 flex flex-col gap-6'>
                         <div className="relative max-w-[40rem]">
                             <label htmlFor="priprema" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Koraci pripreme</label>
-                            <textarea id="priprema" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Opiši pripremu..." required></textarea>
+                            <textarea onChange={e=> setDescriptiom(e.target.value)} id="priprema" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Opiši pripremu..." required></textarea>
                         </div>
                         <div className="relative max-w-[40rem]">
                             <label htmlFor="vrijeme_kuhanja" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Vrijeme kuhanja u minutama: </label>
-                            <input type="number" id="vrijeme_kuhanja" min="0" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 max-w-[15rem]" placeholder="45" required/>
+                            <input onChange={e=> setCookinTime(e.target.value)} type="number" id="vrijeme_kuhanja" min="0" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 max-w-[15rem]" placeholder="45" required/>
                         </div>
                         <div className="relative max-w-[40rem]">
                             <Select
@@ -133,7 +180,7 @@ function NewRecipeForm() {
                         <div className='max-h-[20rem]'>
 
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="multiple_files">Dodajte slike</label>
-                            <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file" multiple/>
+                            <input onChange={e=> setImages(e.target.files[0])} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file" multiple/>
                             <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF.</p>
                         </div>
                         <div>
@@ -144,7 +191,7 @@ function NewRecipeForm() {
                     </div>
                 </div>
                 <div className="flex gap-4 justify-center w-1/3">
-                    <Button type="submit" className='w-full text-white bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 '>
+                    <Button onChange={e=> setVideo(e.target.files[0])}type="submit" className='w-full text-white bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 '>
                         Objavi recept
                     </Button>
                 </div>
--- IzvorniKod/frontend/cookbooked-frontend/src/components/SavedRecipesDisplay.jsx
+++ IzvorniKod/frontend/cookbooked-frontend/src/components/SavedRecipesDisplay.jsx
@@ -1,7 +1,39 @@
+import { useState,useEffect } from "react";
+import axios from 'axios';
 import RecipeList from "./RecipeList";
+
 
 function SavedRecipesDisplay() {
 
+  function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  }
+  useEffect(()=>{
+    let tokenPayload=parseJwt(localStorage.getItem('user_ret'))
+    let userId=tokenPayload.id
+    axios.get('http://localhost:8080/api/v1/users/'+userId+'/bookmarked-recipes',{
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('user_ret')}`,
+            
+          },
+    })
+    .then(response =>{
+      let tmpRcipeList=response.data.map(recept=>({
+        id : recept.id, ime:recept.title, kategorija:recept?.category?.name, vrijeme: recept.cookingTime}))
+        setRecipes(tmpRcipeList)
+    })
+    .catch(err=>{
+        console.error('Error fetching data:', err);
+    })
+
+},[])
+  const [recipes,setRecipes] = useState([])
     const recepti = [
         {
           id: "1",
